f436f0fec52b0a1ce47a89b2c2d5c12116d6fe54
Cris <crisvdnolle@hotmail.com>
2020-04-28 21:34:33 +0200

    Added first functions of interpreter.
    Added is_whitespace function, added tokenize function.

---
+++ interpreter.py
@@ -0,0 +1,42 @@
+from typing import List, TypeVar
+import re
+
+my_tokens = []
+
+class Token:
+    def __init__(self, ty, value):
+        self.type = ty
+        self.value = value
+
+    def __str__(self):
+        return f"Token(type='{self.type}',value={self.value}')"
+
+    __repr__ = __str__
+
+# insert haskell
+
+
+def get_token(token: str) -> Token:
+    if token.isdigit():
+        return Token(ty='INTEGER', value=token)
+    elif token == '+':
+        return Token(ty='ADD', value='+')
+    elif token == '-':
+        return Token(ty='SUBTRACT', value='-')
+
+
+# insert haskell meuk hier
+
+def is_whitespace(token: str) -> bool:
+    if token == ' ':
+        return False
+    else:
+        return True
+
+
+def tokenize(tokens: str) -> List[Token]:
+    if len(tokens) == 0:
+        return []
+    else:
+        head, *tail = filter(is_whitespace, tokens)
+        return [get_token(head)] + tokenize(tail)
