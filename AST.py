c6cd85a205f5391d4af9d551c35322623995b4a4
Cris <crisvdnolle@hotmail.com>
2020-05-06 15:27:27 +0200

    Edited AST

--- AST.py
+++ AST.py
@@ -1,3 +1,8 @@
+class AST:
+    def __init__(self, data):
+        self.top = data
+
+
 class Node:
     def __init__(self, data):
         self.root = data
@@ -8,19 +13,17 @@ class Node:
         return f"Token(name: '{self.root}', left: '{self.lhs}', right: '{self.rhs}', operator: '')"
 
 
-class AST:
-    def __int__(self, data):
-        self.top = data
-
-    def insert(self, data):
-        self.top = self.recursAST(self.top, data)
+# Insert node into ast.
+def insert(ast: AST, data):
+    ast.top = recurs_ast(ast, ast.top, data)
 
 
-def recursAST(ast: AST, node: Node, data):
+# Insert node into correct position of tree.
+def recurs_ast(ast: AST, node: Node, data):
     if node is None:
         node = Node(data)
     elif ast.top.root > data:
-        ast.lhs = recursAST(ast, node.lhs, data)
+        ast.lhs = recurs_ast(ast, node.lhs, data)
     elif ast.top.root < data:
-        node.rhs = recursAST(ast, node.rhs, data)
+        node.rhs = recurs_ast(ast, node.rhs, data)
     return node
