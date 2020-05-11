54746bd5bf6e42318e5561670de12577484ca8cf
Cris <crisvdnolle@hotmail.com>
2020-05-11 14:12:23 +0200

    Added line, pos to tokens.

--- Tokens.py
+++ Tokens.py
@@ -1,24 +1,26 @@
 class Token:
-    def __init__(self, ty, value):
+    def __init__(self, ty, value, line=0, pos=0):
         self.type = ty
         self.value = value
+        self.line = line
+        self.pos = pos
 
     def __str__(self):
         return f"Token(type: {self.type}, value: {self.value})"
 
     def __repr__(self):
-        return f"Token(type: {self.type}, value: {self.value})"
+        return f"Token(type: {self.type}, value: {self.value}, line: {self.line}, pos: {self.pos})"
 
 
 class IntegerToken(Token):
-    def __init__(self, ty, value):
-        super().__init__(ty, value)
+    def __init__(self, ty, value, line=0, pos=0):
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"IntegerToken(type: {self.type}, value: {self.value})"
 
     def __repr__(self):
-        return f"IntegerToken(type: {self.type}, value: {self.value})"
+        return f"IntegerToken(type: {self.type}, value: {self.value}, line: {self.line}, pos: {self.pos})"
 
 
 class OperatorToken(Token):
@@ -38,96 +40,96 @@ class SecondPrecedenceToken(PrecedenceToken):
 
 
 class MultiplyToken(FirstPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"MultiplyToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"MultiplyToken(type: {self.type}, id: {self.ident})"
+        return f"MultiplyToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class DivideToken(FirstPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"DivideToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"DivideToken(type: {self.type}, id: {self.ident})"
+        return f"DivideToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class LParenToken(FirstPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"LParenToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"LParenToken(type: {self.type}, id: {self.ident})"
+        return f"LParenToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class RParenToken(FirstPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"RParenToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"RParenToken(type: {self.type}, id: {self.ident})"
+        return f"RParenToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class AdditionToken(SecondPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"AdditionToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"AdditionToken(type: {self.type}, id: {self.ident})"
+        return f"AdditionToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class SubtractToken(SecondPrecedenceToken):
-    def __init__(self, ty, value, ident):
+    def __init__(self, ty, value, ident, line, pos):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"SubtractToken(type: {self.type}, id: {self.ident})"
 
     def __repr__(self):
-        return f"SubtractToken(type: {self.type}, id: {self.ident})"
+        return f"SubtractToken(type: {self.type}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class VariableToken(Token):
-    def __init__(self, ty, value, ident=None):
+    def __init__(self, ty, value, line=0, pos=0, ident=None):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"VariableToken(type: {self.type}, value: {self.value}, id: {self.ident})"
 
     def __repr__(self):
-        return f"VariableToken(type: {self.type}, value: {self.value}, id: {self.ident})"
+        return f"VariableToken(type: {self.type}, value: {self.value}, id: {self.ident} line: {self.line}, pos: {self.pos})"
 
 
 class AssignmentToken(OperatorToken):
-    def __init__(self, ty, value, ident=None):
+    def __init__(self, ty, value, line, pos, ident=None,):
         self.ident = ident
-        super().__init__(ty, value)
+        super().__init__(ty, value, line, pos)
 
     def __str__(self):
         return f"AssignmentToken(type: '{self.type}', operator: {self.ident} )"
 
     def __repr__(self):
-        return f"AssignmentToken(type: '{self.type}', operator: {self.ident} )"
+        return f"AssignmentToken(type: '{self.type}', operator: {self.ident} line: {self.line}, pos: {self.pos})"
